7431965429fba4e3aceeb1c4611fe8b9196c26c1
chagay levi <chagay88@gmail.com>
2020-09-09 18:42:48 +0300

    login and connecting to mongo

--- project_smart_distribution/server.js
+++ project_smart_distribution/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const http = require('http')
-const socketIO = require('socket.io')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const Data = require('./data');
+const Data = require('./model/data');
+const MyData = require('./model/myData');
 var cors = require('cors');
 
 // our localhost port
@@ -16,10 +16,11 @@ const app = express()
 const server = http.createServer(app)
 
 // This creates our socket using the instance of the server
-const io = socketIO(server)
 // this is our MongoDB database
-const dbRoute =
-  'mongodb+srv://5xCl4aayxNprGQNn:<password>@cluster0.0muup.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const dbRoute ='mongodb+srv://chagay_asher:chagay_asher@cluster0.8uyef.mongodb.net/Cluster0?retryWrites=true&w=majority';
+
+// 'mongodb+srv://5xCl4aayxNprGQNn:chagay87@cluster0.0muup.mongodb.net/test';
+  // 'mongodb+srv://5xCl4aayxNprGQNn:chagay87@cluster0.0muup.mongodb.net/Cluster0?retryWrites=true&w=majority';
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 
@@ -92,22 +93,5 @@ router.post('/putData', (req, res) => {
 app.use('/api', router);
 module.exports = router ;
 
-// This is what the socket.io syntax is like, we will work this later
-io.on('connection', socket => {
-  
-
-  // just like on the client side, we have a socket.on method that takes a callback function
-  socket.on('message', (message) => {
-    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
-    // we make use of the socket.emit method again with the argument given to use from the callback function above
-    console.log('message: ', message)
-    io.sockets.emit('message', message)
-  })
-
-  // disconnect is fired when a client leaves the server
-  socket.on('disconnect', () => {
-    
-  })
-})
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ project_smart_distribution/views/src/Components/Home/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
